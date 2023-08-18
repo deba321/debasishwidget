b5b0874000807ea7484f65d5c3d4cff75bb6124f
Debasish Sarkar <debasish@lyik.com>
2023-08-18 12:22:18 +0530

    made my widget

---
+++ lib/debasishwidget.dart
@@ -0,0 +1,7 @@
+library debasishwidget;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/src/config.dart
@@ -0,0 +1,8 @@
+import 'package:lyik_platform/lyik_platform.dart';
+
+const String pluginId = "lyik-002";
+const String pluginName = "Sample Plugin 2";
+const String storeName = "lyik-002-store";
+
+/// plugin instance
+final Plugin plugin = Plugin(name: pluginName, id: pluginId);
